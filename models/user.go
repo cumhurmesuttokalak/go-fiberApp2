a406f0c252bfe1c0f1155308ad77a58610c513cb
cumhurmessuttokalak <79723267+cumhurmessuttokalak@users.noreply.github.com>
2022-10-17 12:26:53 +0300

    Setuproutes UserCrud

--- models/user.go
+++ models/user.go
@@ -1,14 +1,11 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	CreatedAt time.Time `json:"cAt,omitempty"`
-	FirstName string    `json:"fname,"`
-	LastName  string    `json:"lastname,omitempty"`
+	FirstName string `json:"fname,"`
+	LastName  string `json:"lastname,omitempty"`
 }
